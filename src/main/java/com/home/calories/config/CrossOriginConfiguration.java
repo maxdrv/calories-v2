4dd51537e18f2b44663eeddf98883fa60820a5af
maxdrv <maxdrv.stud@gmail.com>
2022-07-31 16:54:22 +0300

    added CORS configuration

---
+++ src/main/java/com/home/calories/config/CrossOriginConfiguration.java
@@ -0,0 +1,23 @@
+package com.home.calories.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrossOriginConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/baseProducts").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+
+}
