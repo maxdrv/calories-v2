1abfa3796828232ccfeeb3a2f1a514d7cdf505c4
maxdrv <maxdrv.stud@gmail.com>
2022-08-07 18:09:22 +0300

    added data base clean up test extension

---
+++ src/test/java/com/home/calories/test/TruncateExtension.java
@@ -0,0 +1,27 @@
+package com.home.calories.test;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.nio.charset.StandardCharsets;
+
+public class TruncateExtension implements BeforeEachCallback {
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        ApplicationContext appContext = SpringExtension.getApplicationContext(context);
+
+        JdbcTemplate template = appContext.getBean(JdbcTemplate.class);
+
+        Resource resource = appContext.getResource("classpath:truncate.sql");
+
+        String sql = IOUtils.toString(resource.getInputStream(), StandardCharsets.UTF_8);
+
+        template.execute(sql);
+    }
+}
---
+++ src/test/resources/truncate.sql
@@ -0,0 +1,4 @@
+truncate table base_products;
+
+alter sequence base_products_seq restart 10000;
+
