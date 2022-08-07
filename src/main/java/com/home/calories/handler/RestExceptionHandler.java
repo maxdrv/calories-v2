f590836d55bd9e5de79ba404cf1ba90a4d927730
maxdrv <maxdrv.stud@gmail.com>
2022-08-07 17:38:01 +0300

    added Postgres Driver and properties for datasource

---
+++ src/main/java/com/home/calories/handler/RestExceptionHandler.java
@@ -0,0 +1,64 @@
+package core.handlers;
+
+import core.dto.errors.ErrorDetail;
+import core.dto.errors.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException manve, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(System.currentTimeMillis());
+        errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorDetail.setTitle("Validation Failed");
+        errorDetail.setDetail("Input validation failed");
+        errorDetail.setDeveloperMessage(manve.getClass().getName());
+
+        List<FieldError> fieldErrors = manve.getBindingResult().getFieldErrors();
+        for (FieldError fe : fieldErrors) {
+
+            List<ValidationError> validationErrorList = errorDetail.getErrors()
+                    .computeIfAbsent(fe.getField(), k -> new ArrayList<>());
+            ValidationError validationError = new ValidationError();
+            validationError.setCode(fe.getCode());
+            validationError.setMessage(messageSource.getMessage(fe, Locale.ENGLISH));
+            validationErrorList.add(validationError);
+        }
+        return handleExceptionInternal(manve, errorDetail, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(System.currentTimeMillis());
+        errorDetail.setStatus(status.value());
+        errorDetail.setTitle("Message Not Readable");
+        errorDetail.setDetail(ex.getMessage());
+        errorDetail.setDeveloperMessage(ex.getClass().getName());
+
+        return handleExceptionInternal(ex, errorDetail, headers, status, request);
+    }
+
+}
